16b811bf4aa0db23cf38c0e15081cc0e07f4d561
Sasha Lišková <sliskova@proton.me>
2024-03-06 22:31:39 -0500

    Add GPL to all sourcec files.

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2023, 2024 Sasha Lišková and Stephanie Beckon
+ *
+ * This file is part of Transpapers.
+ *
+ * Transpapers is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Transpapers is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Transpapers. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { Name, DateFormat, DateFormatPart } from '../types/types';
 import { Person } from '../types/person';
 
