61d22e4a6922a268f33dd4564630ce63f96a5566
Katie Lišková <avkl@lavabit.com>
2025-05-10 12:33:42 -0400

    Update packages; migrate to `eslint-config-love` and eslint v9
    The package `eslint-config-airbnb` is functionally unmaintained, cf.
    https://github.com/airbnb/javascript/issues/2961.

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { 'jsx-a11y': { rules: jsxA11y.rules, }, }, rules:jsxA11y.configs.recommended.rules, },
+  {
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+  },
+]);
