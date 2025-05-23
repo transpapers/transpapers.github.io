303ded7ff9584998547a865d6ba9b9def4e8683e
SRB-Paljor <sbeckon2017@gmail.com>
2025-05-23 09:10:49 -0400

    Finalized guide sections for Oregon.

---
+++ src/components/guides/Oregon/BirthCertUpdate.tsx
@@ -0,0 +1,127 @@
+/**
+ * Copyright 2023, 2024 Sasha Lišková and Stephanie Beckon
+ *
+ * This file is part of Transpapers.
+ *
+ * Transpapers is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Transpapers is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Transpapers. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import * as React from "react";
+
+import { type Person } from "../../../types/person";
+
+function OregonBirthCertUpdateGuide({ person }: { person: Person }) {
+  const { age, parentsAreOkay, birthJurisdiction } = person;
+
+  return (
+    <section key="Oregon-BC-Update">
+      <h3>Birth Certificate Update (OR)</h3>
+
+      
+      {age && age < 18 ? (
+        <>
+          <p>
+            {birthJurisdiction && birthJurisdiction === "Oregon" ? (
+              " A court case is not needed to update your information, instead it can be updated by changing your birth certificate information and updating everything else using that. "
+            ) : ("")}
+            The form used to update a birth certificate is the "Application to Change the Name
+            and/or Sex on a Record of Live Birth to Support Gender Identity". If your 
+            parents/guardians mailing address is different from their listed address they will 
+            need to put it in number 3. They will also need to fill out items 6 and 15. 
+            Do <strong>not</strong> sign/date the form until a notary instructs you to do so. 
+          </p>
+
+          <p>
+            Notaries can be found in court buildings, banks,{" "}
+            <a href="https://www.onpointcu.com/faq/do-you-offer-notary-services/">OnPoint Credit Unions</a>
+            , some <a href="https://www.theupsstore.com/tools/find-a-store">UPS Locations</a>
+            , or{" "} <a href="https://www.notarize.com/">online</a>. There may
+            be a fee for this required service.
+          </p>
+
+          <p>
+            After the form is notarized your parent/guardian will need a large envelope,
+            {birthJurisdiction && birthJurisdiction === "Oregon" ? (" ") : (
+                " a copy of your court order, ")}
+            a photocopy of their ID (both sides), and payment. If your parent is filling this 
+            out and their name does not match the one on your birth certificate they will need to 
+            attach documentation of the change (e.g. marriage certificate, divorce certificate, 
+            court order, etc…) <strong>photocopies only</strong>.
+            {parentsAreOkay && parentsAreOkay === true ? ("") : (
+                " Legal guardians need to provide proof of guardianship. "
+            )}
+            If they do not have a driver's license or state ID the “Oregon Vital Records 
+            Acceptable Proofs of Identity” we included has a list of acceptable alternatives 
+            to photocopy. For payment they take either checks or money orders made out to 
+            “Oregon Vital Records”. Mail the application to the following address:
+          </p>
+        </>
+      ) : (
+        <>
+          <p>
+            The form used to update a birth certificate is the “Application to Change the Name 
+            and/or Sex on a Record of Live Birth to Support Gender Identity”. If your mailing 
+            address is different from your listed address put it in number 3. You will then need 
+            to fill out item 15, specifically the number of copies that you want and what form 
+            you want them in. Do <strong>not</strong> sign/date the form until a notary 
+            instructs you to do so.
+          </p>
+
+          <p>
+            Notaries can be found in court buildings, banks,{" "}
+            <a href="https://www.onpointcu.com/faq/do-you-offer-notary-services/">OnPoint Credit Unions</a>
+            , some <a href="https://www.theupsstore.com/tools/find-a-store">UPS Locations</a>
+            , or{" "} <a href="https://www.notarize.com/">online</a>. There may
+            be a fee for this required service.
+          </p>
+
+          <p>
+            After your form is notarized you will need a large envelope, a copy of your court 
+            order, a photocopy of your ID (both sides), and payment. If you do not have a \
+            driver's license or state ID the “Oregon Vital Records Acceptable Proofs of Identity” 
+            we included has a list of acceptable alternatives to photocopy. For payment they take 
+            either checks or money orders made out to “Oregon Vital Records”. Mail the 
+            application to the following address:
+          </p>
+        </>
+      )}
+
+      <p>
+        <span>Oregon Vital Records</span>
+        <br />
+        <span>PO Box 14050</span>
+        <br />
+        <span>Portland, OR 97293</span>
+      </p>
+
+      <p>
+        Alternatively the letter can be placed in a dropbox at 800 NE Oregon St, Portland. 
+        It may take up to six months to process once the letter is sent.
+        {age && age < 18 ? (
+          <>
+            {birthJurisdiction && birthJurisdiction === "Oregon" ? (
+              " According to the Social Security Administration's " +
+              <a href="https://secure.ssa.gov/poms.nsf/lnx/0110212090">rules</a> + 
+              " when the new birth certificate and correspondence letter arrive a " +
+              " parent/guardian can update your information at Social Security using " +
+              " those documents. Ignore any mention of the court order in those guides."
+            ) : ("")}
+          </>
+        ) : ("")}
+      </p>
+    </section>
+  );
+}
+
+export default OregonBirthCertUpdateGuide;
---
+++ src/components/guides/Oregon/EverythingElse.tsx
@@ -0,0 +1,138 @@
+/**
+ * Copyright 2023, 2024 Sasha Lišková and Stephanie Beckon
+ *
+ * This file is part of Transpapers.
+ *
+ * Transpapers is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Transpapers is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Transpapers. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import * as React from "react";
+
+function OregonEverythingElseGuide() {
+  return (
+    <section key="OR-EverythingElse">
+      <h3>Everything Else (OR)</h3>
+      <p>
+        Once you have the court order and primary identification in your new
+        name, you can change your name almost everywhere else without issue.
+        Some places will even allow for digital updates by scanning in the ID
+        and court order. What follows is a list of places, in no particular
+        order, where you may want to update your name. Any forms that you fill
+        out should be signed in your new name.
+      </p>
+
+      <p>
+        <strong>Bank</strong>: Bring your new ID and court order and request the
+        name on your account(s) be changed. If you have a debit card or
+        checkbook out of this account, then you will need to request new ones.
+        Any joint account holders must also be present to sign.
+      </p>
+      <p>
+        <strong>Credit Card</strong>: Most creditors will require you to snail
+        mail or fax a photocopy of your ID and court order. If a particular
+        company is stubborn in updating the account name, consider canceling the
+        card and opening a new card with them or another provider.
+      </p>
+      <p>
+        <strong>Work</strong>: If you have an HR system, bring your new ID, new
+        social security card, and court order to them and ask them to update
+        your name. If you get your health insurance through your work, you can
+        have them send the updated information to them on your behalf.
+      </p>
+      <p>
+        <strong>Healthcare Providers</strong>: You should be able to bring in
+        only your new ID at your next in-person visit and go to the front
+        secretaries to have your name updated in the system. Be aware that
+        certain medical professionals will need to know your transition status.
+      </p>
+      <p>
+        <strong>Gas/Heating/Electricity Provider</strong>: Should be
+        able to be done by phone, email, or online.
+      </p>
+      <p>
+        <strong>Water/Sewer</strong>: Should be able to be done by phone, email,
+        or snail mail.
+      </p>
+      <p>
+        <strong>Internet Provider</strong>: Should be able to be done online.
+      </p>
+      <p>
+        <strong>Garbage Service</strong>: Should be able to be done by email or
+        snail mail.
+      </p>
+      <p>
+        <strong>Mortgage</strong>: Should be able to be done by email or snail
+        mail.
+      </p>
+      <p>
+        <strong>Property/House Title</strong>: Contact your local county 
+        Recorder's Office. A Grant or Quitclaim deed will need to be filed. 
+        Bring ID and court order.
+      </p>
+      <p>
+        <strong>Phone Service</strong>: Go to the nearest store of your carrier
+        network with your new ID and court order and request your information be
+        updated.
+      </p>
+      <p>
+        <strong>Taxes (IRS)</strong>: The IRS will be informed of your name
+        change when you update your information with Social Security. You do <strong>not</strong>
+        {" "}need to contact them.
+      </p>
+      <p>
+        <strong>Primary School Records</strong>: Each school will have different
+        requirements and protocols; contact them and see what theirs is. Be
+        aware that Michigan law does not require your high school to update your
+        educational records, so it is possible that they may refuse.
+      </p>
+      <p>
+        <strong>College Records</strong>: Contact the Student Records Department
+        of your university. Your court order and new ID should work. You may
+        also consider asking about updating your school profile and email.
+      </p>
+      <p>
+        <strong>Insurance</strong>: Contact your insurer(s). Requirements will
+        vary.
+      </p>
+      <p>
+        <strong>Government Assistance</strong>: Contact your assistance agency
+        to update their case file. You should only need a primary ID and court
+        order.
+      </p>
+      <p>
+        <strong>Voter Registration</strong>: You can update online at this{" "}
+        <a href="https://secure.sos.state.or.us/orestar/vr/showVoterSearch.do?lang=eng&source=SOS">link</a>
+        {" "}or by completing the Oregon Voter Registration Card (SEL 500) and 
+        hand delivering it to your county’s local election office. If there are 
+        only a few days until an election that you want to vote in we recommend 
+        the second option. We have attached a partially filled form to this guide 
+        if you need it, fill in any blanks on page 1. Page 2 contains the 
+        addresses for all local election offices.
+      </p>
+      <p>
+        <strong>Some other places to consider:</strong>
+      </p>
+      <ul>
+        <li>Retirement account</li>
+        <li>Clubs/memberships</li>
+        <li>Municipal tax authorities</li>
+        <li>Online payment services (Venmo, Paypal, etc…)</li>
+        <li>Public transit accounts</li>
+        <li>Monthly subscriptions (Netflix, Hulu, etc…)</li>
+      </ul>
+    </section>
+  );
+}
+
+export default MichiganEverythingElseGuide;
