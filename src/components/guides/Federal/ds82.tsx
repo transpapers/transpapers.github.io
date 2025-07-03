545f3886e075376869dd44520b2e10c80c385136
SRB-Paljor <sbeckon2017@gmail.com>
2025-07-03 07:46:48 -0400

    Adjusted DS-82 form for preflight error.

--- src/components/guides/Federal/ds82.tsx
+++ src/components/guides/Federal/ds82.tsx
@@ -24,7 +24,7 @@ import { type Person } from "../../../types/person";
 // TODO Give this a once-over for style.
 // TODO Unify this with the other two passport forms.
 function DS82Guide({ person }: { person: Person }) {
-  const { isChangingLegalSex } = person;
+  const { age, isChangingLegalSex } = person;
   return (
     <section key="Federal-Passport">
       <h3>Obtaining Your Passport</h3>
@@ -52,11 +52,12 @@ function DS82Guide({ person }: { person: Person }) {
         in every box.
       </p>
 
-        {isChangingLegalSex ? (
+      {isChangingLegalSex ? (
         <p>
-          The &ldquo;Attestation of Orr v. Trump Class Membership&rdquo; form is 
-          new and allows for updating gender markers once again. To complete the 
-          form simply sign on page 2.
+          The &ldquo;Attestation of Orr v. Trump Class Membership&rdquo; form is new and allows
+          for updating gender markers once again. To complete the form simply
+          {age && age < 18 ? (" have a parent/guardian sign ") : (" sign ")}
+          on page 2.
         </p>
       ):("")}
 
