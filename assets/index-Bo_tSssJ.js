66b9a5919b9b6e5a99a27d6570966977511c073e
SRB-Paljor <SRB-Paljor@users.noreply.github.com>
2025-08-22 15:49:54 +0000

    deploy: f0a87f463784e6c0ad3198d8ce546389a2f2cbc3

--- assets/index-Bo_tSssJ.js
+++ assets/index-Bo_tSssJ.js
@@ -32338,7 +32338,6 @@ function RhodeIslandEverythingElseGuide() {
 }
 function RhodeIslandFilingGuide({
   person
-  //residentLocality,
 }) {
   const { age, residentJurisdictionName, residentLocalityName } = person;
   const residentJurisdiction = allJurisdictions.find(
@@ -34787,54 +34786,61 @@ function NewYorkEverythingElseGuide() {
   ] }, "NY-EverythingElse");
 }
 function NewYorkFilingGuide({
-  person,
-  locality
+  person
 }) {
-  const { age } = person;
-  const { name, isNYC, borough, court } = locality;
-  return /* @__PURE__ */ jsxRuntimeExports.jsxs("section", { children: [
-    /* @__PURE__ */ jsxRuntimeExports.jsx("h3", { children: "Filing Initial Forms (NY)" }),
-    isNYC ? /* @__PURE__ */ jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, { children: [
-      /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
-        age && age < 18 ? "A parent/guardian " : "You ",
-        " can file in any of the 5 NYC Civil courts. We have them listed below with your borough bolded."
-      ] }),
-      ...Array.from(
-        newYork.localities.filter((loc) => loc.isNYC).map(({ court: court2 }) => /* @__PURE__ */ jsxRuntimeExports.jsxs("span", { children: [
-          /* @__PURE__ */ jsxRuntimeExports.jsx("strong", { children: borough }),
-          ": ",
-          court2.address,
-          ". Phone Number:",
+  const { age, residentJurisdictionName, residentLocalityName } = person;
+  const residentJurisdiction = allJurisdictions.find(
+    (j) => j.name === residentJurisdictionName
+  );
+  if (residentJurisdiction) {
+    const localities = residentJurisdiction.localities;
+    const residentLocality = localities.find(
+      (j) => j.name === residentLocalityName
+    );
+    return /* @__PURE__ */ jsxRuntimeExports.jsxs("section", { children: [
+      /* @__PURE__ */ jsxRuntimeExports.jsx("h3", { children: "Filing Initial Forms (NY)" }),
+      residentLocality.isNYC ? /* @__PURE__ */ jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, { children: [
+        /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
+          age && age < 18 ? "A parent/guardian " : "You ",
+          " can file in any of the 5 NYC Civil courts. We have them listed below."
+        ] }),
+        ...Array.from(
+          newYork.localities.filter((loc) => loc.isNYC).map(({ court, borough }) => /* @__PURE__ */ jsxRuntimeExports.jsxs("span", { children: [
+            /* @__PURE__ */ jsxRuntimeExports.jsx("strong", { children: borough }),
+            ": ",
+            court.address,
+            ". Phone Number:",
+            " ",
+            court.phone
+          ] }, "{court.name}"))
+        ),
+        /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
+          residentLocality.court.specificCourtInfo ?? "",
+          "The cost to file is currently $65. If you bring cash make sure it's exact as they probably won't make change for you."
+        ] })
+      ] }) : /* @__PURE__ */ jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, { children: [
+        /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
+          "The filing location is the ",
+          residentLocality.name,
+          " county court at ",
+          residentLocality.court.address,
+          ".",
+          age && age < 18 ? "A parent/guardian " : "You ",
+          " may want to call ahead to check accepted payment types, their phone number is",
           " ",
-          court2.phone
-        ] }, "{court.name}"))
-      ),
-      /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
-        court.specificCourtInfo ?? "",
-        "The cost to file is currently $65. If you bring cash make sure it's exact as they probably won't make change for you."
-      ] })
-    ] }) : /* @__PURE__ */ jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, { children: [
-      /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
-        "The filing location is the ",
-        name,
-        " county court at ",
-        court.address,
-        ".",
-        age && age < 18 ? "A parent/guardian " : "You ",
-        " may want to call ahead to check accepted payment types, their phone number is",
-        " ",
-        court.phone,
-        ". Generally speaking though as long as you have cash, check, and card all ready to go then you are good. The cost to file is currently $210, there will not be a hearing."
+          residentLocality.court.phone,
+          ". Generally speaking though as long as you have cash, check, and card all ready to go then you are good. The cost to file is currently $210, there will not be a hearing."
+        ] }),
+        residentLocality.court.specificCourtInfo && /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: residentLocality.court.specificCourtInfo })
       ] }),
-      court.specificCourtInfo && /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: court.specificCourtInfo })
-    ] }),
-    /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
-      /* @__PURE__ */ jsxRuntimeExports.jsx("strong", { children: "By state law, court clerks are barred from answering questions about the forms." }),
-      " ",
-      "We recommend that you direct any questions you may have to the court’s legal assistance center, a local LGBT organization, or an attorney."
-    ] }),
-    age && age < 18 ? /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: "After the clerk receives the filled out & notarized petition, your parent/guardian(s) ID, the certified copy of the minors birth certificate, and the filing fee (or optional fee waiver) they should then give a reference number. The clerk should also tell your parent/guardian(s) when to come back for the court order confirming the changes. There shouldn’t be a hearing unless there is a parent/guardian who refused to give consent for the change. When they return they only need their ID(s) and the reference slip that the clerk gave them. Finally a document called “Order Granting Name Change and/or Sex Designation Change for an Individual Minor” (UCS-NC02) will be given that we will refer to as the court order. We recommend requesting and paying for an additional certified copy or 2 of the order for later use. Have the original kept in a safe place." }) : /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: "After the clerk recieves your notarized UCS-NC1 form, your ID, a copy of your birth certificate, and the filing fee (or optional fee waiver) they should give you a reference number. They will also tell you when to return for your court order confirming the changes as there will not be a hearing. When you return you only need your ID and the reference slip they gave you. You should recieve a document called “Order Granting Name Change and/or Sex Designation Change for an Individual Adult” (UCS-NC01) that we will refer to as the court order. We recommend paying for an additional certified copy or 2 for later use. Keep the original in a safe space." })
-  ] }, "NewYork-Filing");
+      /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
+        /* @__PURE__ */ jsxRuntimeExports.jsx("strong", { children: "By state law, court clerks are barred from answering questions about the forms." }),
+        " ",
+        "We recommend that you direct any questions you may have to the court’s legal assistance center, a local LGBT organization, or an attorney."
+      ] }),
+      age && age < 18 ? /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: "After the clerk receives the filled out & notarized petition, your parent/guardian(s) ID, the certified copy of the minors birth certificate, and the filing fee (or optional fee waiver) they should then give a reference number. The clerk should also tell your parent/guardian(s) when to come back for the court order confirming the changes. There shouldn’t be a hearing unless there is a parent/guardian who refused to give consent for the change. When they return they only need their ID(s) and the reference slip that the clerk gave them. Finally a document called “Order Granting Name Change and/or Sex Designation Change for an Individual Minor” (UCS-NC02) will be given that we will refer to as the court order. We recommend requesting and paying for an additional certified copy or 2 of the order for later use. Have the original kept in a safe place." }) : /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: "After the clerk recieves your notarized UCS-NC1 form, your ID, a copy of your birth certificate, and the filing fee (or optional fee waiver) they should give you a reference number. They will also tell you when to return for your court order confirming the changes as there will not be a hearing. When you return you only need your ID and the reference slip they gave you. You should recieve a document called “Order Granting Name Change and/or Sex Designation Change for an Individual Adult” (UCS-NC01) that we will refer to as the court order. We recommend paying for an additional certified copy or 2 for later use. Keep the original in a safe space." })
+    ] }, "NewYork-Filing");
+  }
 }
 /*!
  * @licstart The following is the entire license notice for the JavaScript code in this file.
