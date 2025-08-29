162a4bf05e5341dd452e4e74233ec004c3028ea1
SRB-Paljor <SRB-Paljor@users.noreply.github.com>
2025-08-29 15:44:19 +0000

    deploy: b660cbeac7656d25b80d99e0296a3eb8d058e0f0

--- assets/index-FV-9N6Nq.js
+++ assets/index-FV-9N6Nq.js
@@ -38136,7 +38136,7 @@ function AlaskaBirthCertificateGuide({ person }) {
       " use cash for the fee unless you are hand delivering it."
     ] }),
     isChangingLegalSex ? /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
-      residentJurisdictionName === "Alaska" ? /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: "To get an amended sex designation you will need a letter from the person giving you trans related care." }) : /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: "To get an amended sex designation you will either need a letter from the person giving you trans related care or a court ordered gender change." }),
+      residentJurisdictionName && residentJurisdictionName === "Alaska" ? /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: "To get an amended sex designation you will need a letter from the person giving you trans related care." }) : /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: "To get an amended sex designation you will either need a letter from the person giving you trans related care or a court ordered gender change." }),
       " ",
       "We have included a template letter with a list of qualifying professions to take to this person for their reference. Email “BVSSpecialServices@alaska.gov” with any questions."
     ] }) : "",
