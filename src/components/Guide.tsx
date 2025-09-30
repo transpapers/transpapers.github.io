33a5559bb8d0ffc3c0b3d83852311bd1e77731a4
sbeck <sbeck@ARELLA>
2025-09-30 13:13:04 -0400

    Switching to residentJurisdiction for test

--- src/components/Guide.tsx
+++ src/components/Guide.tsx
@@ -163,7 +163,7 @@ function Guide() {
     }
   }, console.error);
 
-  const locality = applicant.residentLocality;
+  const locality = applicant.residentJurisdiction;
 
   const guideElements = [];
 
