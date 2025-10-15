5e6589a74545d0854a695aa2d0112eb8f6a1550b
SRB-Paljor <SRB-Paljor@users.noreply.github.com>
2025-10-15 12:02:07 +0000

    deploy: 5882d9171921c99d80a6067b64ee3d8b783ed201

--- assets/index-FeIY4BXl.js
+++ assets/index-FeIY4BXl.js
@@ -78417,11 +78417,10 @@ function fillField(doc, field) {
 function realLocation(field, pageHeight, ourDpi) {
   const trueHeight = Math.round(pageHeight * (1 / 72) * ourDpi);
   const yAdjustedDPI = Math.round(field.loc.y * (1 / 72) * ourDpi);
-  const xAdjustedDPI = Math.round(field.loc.x * (1 / 72) * ourDpi);
   const defaultFontSize = Math.round(8.64 * (1 / 72) * ourDpi);
   const fontSize = field.font?.fontSize ?? defaultFontSize;
   return {
-    x: xAdjustedDPI,
+    x: field.loc.x,
     y: trueHeight - fontSize - yAdjustedDPI,
     size: fontSize
   };
