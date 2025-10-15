7e18d6a0f252057ef08c6e92af5bd60719fb96c2
SRB-Paljor <SRB-Paljor@users.noreply.github.com>
2025-10-15 14:16:27 +0000

    deploy: 5862854a6d305ebe8d96742a4d63e024ee20bd62

--- assets/index-DAcjKcsO.js
+++ assets/index-DAcjKcsO.js
@@ -36869,6 +36869,60 @@ function OregonFeeWaiverGuide({ person }) {
     ] })
   ] }, "Oregon-Fee-Waiver");
 }
+function OregonFilingInitialFormsGuide({
+  person
+}) {
+  const { age, isChangingLegalSex, residentJurisdictionName, residentLocalityName } = person;
+  const residentJurisdiction = allJurisdictions.find(
+    (j) => j.name === residentJurisdictionName
+  );
+  if (residentJurisdiction) {
+    const localities = residentJurisdiction.localities;
+    const residentLocality = localities.find(
+      (j) => j.name === residentLocalityName
+    );
+    return /* @__PURE__ */ jsxRuntimeExports.jsxs("section", { children: [
+      /* @__PURE__ */ jsxRuntimeExports.jsx("h3", { children: "Filing Initial Forms (OR)" }),
+      residentLocality ? /* @__PURE__ */ jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, { children: [
+        isChangingLegalSex ? /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
+          "Your county's circuit court is at",
+          " ",
+          residentLocality.court.address,
+          ". However the petition can be filed at any other Oregon circuit court as well. We have provided a map of all Oregon circuit courts",
+          " ",
+          /* @__PURE__ */ jsxRuntimeExports.jsx("a", { href: "https://www.google.com/maps/d/u/0/viewer?mid=11l4y7lcd51YLjlNXl2Qcbb4Fmg_1aaI&hl=en&ll=44.22287483137777%2C-120.8304999&z=7", children: "here" }),
+          " ",
+          "and if another court is more safe/convenient then file there instead. Every circuit court in Oregon has their info on this",
+          " ",
+          /* @__PURE__ */ jsxRuntimeExports.jsx("a", { href: "https://www.courts.oregon.gov/courts/Pages/default.aspx", children: "website" }),
+          ". Some courts accept filing by mail or even online, the web link we provided will have the details for those. Make sure to fill in the “County” field at the top of every page on all of the forms with the name of whatever county they will be filed at. If the filing method is in-person or by mail",
+          age && age < 18 ? " your petitioner " : " you ",
+          " will need the petition forms, photo ID, payment, and (optionally) the fee waiver forms. To find out what payment methods the court accepts check the link or call them."
+        ] }) : /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
+          residentLocality.name === "Umatilla" ? " Umatilla county has two valid circuit court locations to file at " + residentLocality.court.address + ". Pick whichever is most convenient. " : " The filing location is the " + residentLocality.name + " circuit court at " + residentLocality.court.address + ". ",
+          "They have a webpage with their open hours and other information at",
+          " ",
+          /* @__PURE__ */ jsxRuntimeExports.jsx("a", { href: residentLocality.court.website, children: residentLocality.court.website }),
+          ". That link may also show what filing methods the court accepts, some will accept filing by mail or even online, others are in-person only. It may also show accepted payment types. If not it will have a phone number to call for questions. If the filing method is in-person or by mail",
+          age && age < 18 ? " your petitioner " : " you ",
+          " will need the petition forms, photo ID, payment, and (optionally) the fee waiver forms."
+        ] }),
+        /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
+          /* @__PURE__ */ jsxRuntimeExports.jsx("strong", { children: "By state law, court clerks are barred from answering questions about how to fill out the forms." }),
+          " ",
+          "We recommend that you direct any of those questions to the court's legal assistance center, a local LGBT organization, or an attorney."
+        ] }),
+        residentLocality.court.specificCourtInfo && /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: residentLocality.court.specificCourtInfo }),
+        age && age < 18 ? /* @__PURE__ */ jsxRuntimeExports.jsxs("p", { children: [
+          "Your petitioner will need to send out written notice to any other parents or legal guardians regardless of custody, the only exceptions being if the minor has not lived with them and they haven't given support (or tried to). You do ",
+          /* @__PURE__ */ jsxRuntimeExports.jsx("strong", { children: "not" }),
+          " ",
+          "need their consent, if they have an issue they can contact the court using the notice. A copy of that notice has been provided; your petitioner simply needs to add the case number. Once notice has been sent your petitioner can fill out the table in the “Declaration of Notice” form with everyone they contacted. At that point they can sign it and send it to the court, then the court will likely make a judgment without a hearing and mail the results. If so skip the “Court Hearing” section. Assuming there isn't a hearing you will need to wait for the “General Judgement”, which we will call the court order, to arrive. Once it does your petitioner will also need to order 2 or 3 certified copies of the court order for use later in this process as some places will not return them. Keep the original in a safe place. If there is a hearing your petitioner will recieve a court date instead and we recommend getting help from a lawyer or our “Resources” section."
+        ] }) : /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: "Once you have filed, the court will likely make a judgment without a hearing and will mail you the results. If so skip the “Court Hearing” section. There are a few exceptions such as a paperwork mishap or some types of criminal record that could cause a hearing though. Assuming there isn't one you will need to wait for the “General Judgement”, which we will call the court order, to arrive. Once it does you will need to order 2 or 3 certified copies of the court order for use later in the process as some places will not return them. Keep the original in a safe place. If there is a hearing you will recieve a court date instead." })
+      ] }) : /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: "Could not generate, residentLocality missing." })
+    ] }, "OR-InitialForms");
+  }
+}
 /*!
  * @licstart The following is the entire license notice for the JavaScript code in this file.
  * Copyright (C) 2023-2025 Sasha Lišková and Stephanie Beckon
@@ -37009,12 +37063,11 @@ const oregonNameChange = {
       map: feeWaiverOregonMap,
       include: (applicant) => !isMinor(applicant) || (applicant.birthJurisdictionName ?? "") !== "Oregon"
     },
-    //{
-    //name: "Filing Initial Documents",
-    //guide: OregonFilingInitialFormsGuide,
-    //include: (applicant) =>
-    //!isMinor(applicant) || (applicant.birthJurisdictionName ?? "") !== "Oregon",
-    //},
+    {
+      name: "Filing Initial Documents",
+      guide: OregonFilingInitialFormsGuide,
+      include: (applicant) => !isMinor(applicant) || (applicant.birthJurisdictionName ?? "") !== "Oregon"
+    },
     {
       name: "Court Hearing",
       guide: OregonCourtHearingGuide,
