b188ee4da73ac99e185c463ac28eaf065c3744b9
SRB-Paljor <sbeckon2017@gmail.com>
2025-10-16 18:32:47 -0400

    Trying different assignment for preflight

--- src/components/Guide.tsx
+++ src/components/Guide.tsx
@@ -120,7 +120,7 @@ function getProcesses(
 }
 
 function Guide() {
-  const applicant = useStore((state) => state.person) as Person;
+  const applicant: Person = useStore((state) => state.person);
 
   const { residentJurisdictionName, birthJurisdictionName, processNames } =
     useStore((state) => state);
