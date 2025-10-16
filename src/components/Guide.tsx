881d231d05685a12cc44369c144bbcc9e23b226b
SRB-Paljor <sbeckon2017@gmail.com>
2025-10-16 10:36:46 -0400

    Switched type declaration to applicant instead.

--- src/components/Guide.tsx
+++ src/components/Guide.tsx
@@ -120,7 +120,7 @@ function getProcesses(
 }
 
 function Guide() {
-  const applicant = useStore((state) => state.person);
+  const applicant = useStore((state) => state.person) as Person;
 
   const { residentJurisdictionName, birthJurisdictionName, processNames } =
     useStore((state) => state);
@@ -163,7 +163,7 @@ function Guide() {
     }
   }, console.error);
 
-  const locality = applicant.residentLocality as AnyLocality;
+  const locality = applicant.residentLocality;
 
   const guideElements = [];
 
