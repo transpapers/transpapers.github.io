508e6ccb547dff5d8839e55a896ab5358a7c8959
SRB-Paljor <sbeckon2017@gmail.com>
2025-10-16 11:21:13 -0400

    Adjusted locality const again.

--- src/components/Guide.tsx
+++ src/components/Guide.tsx
@@ -163,7 +163,7 @@ function Guide() {
     }
   }, console.error);
 
-  const locality = residentJurisdiction.localities;
+  const locality = applicant.residentJurisdictionName;
 
   const guideElements = [];
 
