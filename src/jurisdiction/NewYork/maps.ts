c2ab591198171151d0aa9e383aff4c633029addf
SRB-Paljor <sbeckon2017@gmail.com>
2025-10-20 16:58:33 -0400

    Attempting NY form fix

--- src/jurisdiction/NewYork/maps.ts
+++ src/jurisdiction/NewYork/maps.ts
@@ -780,11 +780,11 @@ export const birthCertNYCMap: Formfill[] = [
     choice: (() => {
       switch (applicant.gender) {
         case GenderMarker.M:
-          return "Choice1";
+          return 1;
         case GenderMarker.F:
-          return "Choice2";
+          return 2;
         case GenderMarker.X:
-          return "Choice3";
+          return 3;
       }
     })(),
   }),
