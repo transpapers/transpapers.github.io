14acd74182e2ba630d010c7856e19e765c3e77c7
SRB-Paljor <sbeckon2017@gmail.com>
2025-10-27 14:42:46 -0400

    Testing alterrnate location for loc

--- src/jurisdiction/NewYork/maps.ts
+++ src/jurisdiction/NewYork/maps.ts
@@ -75,11 +75,11 @@ export const adultNameSexPetitionMap: Formfill[] = [
   }),
   (applicant) => ({
     text: applicant.hasCriminalRecord ? "•" : "",
-    loc: { page: 1, x: 685, y: 64 },
+    loc: { page: 1, x: 685, y: 98 },
   }),
   (applicant) => ({
     text: !applicant.hasCriminalRecord ? "•" : "",
-    loc: { page: 1, x: 753, y: 64 },
+    loc: { page: 1, x: 753, y: 98 },
   }),
   (applicant) => ({
     text: applicant.isChangingLegalName ? applicant.reasonForNameChange : "",
