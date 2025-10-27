657b71ca98da0a078eb91cb7d8ea0d2983f71481
SRB-Paljor <sbeckon2017@gmail.com>
2025-10-27 10:12:03 -0400

    Mimicking RI guide process to get locality properties.

--- src/jurisdiction/RhodeIsland/maps.ts
+++ src/jurisdiction/RhodeIsland/maps.ts
@@ -50,7 +50,8 @@ import { RhodeIslandCityOrTown } from "../../types/locality";
 export const changeOfNameMap: Formfill[] = [
   /*FIXME adjust this or the util function so it works.  */
   (applicant) => ({
-    text: (applicant.residentLocality as RhodeIslandCityOrTown).county ?? "",
+    text: (
+      applicant.residentJurisdiction?.localities.find((j) => j.name === applicant.residentLocalityName) as RhodeIslandCityOrTown)?.county,
     loc: { x: 145, y: 150 },
   }),
   (applicant) => ({
