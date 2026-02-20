4c02635c43544d0a3807e5ff9b8f03c801d1209c
SRB-Paljor <sbeckon2017@gmail.com>
2026-02-20 08:45:20 -0500

    Couldn't use FED abbreviation for test data, used MI instead.

--- src/jurisdiction/all.ts
+++ src/jurisdiction/all.ts
@@ -183,7 +183,7 @@ export const federal: Jurisdiction<Locality> = {
 
 export const elsewhere: Jurisdiction<Locality> = {
   name: "Elsewhere",
-  abbreviation: "FED",
+  abbreviation: "MI",
   processes: [elsewhereBirthRecord],
 
   localities: [],
