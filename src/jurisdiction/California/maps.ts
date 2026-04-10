b50e68457ae330e5a5730f62606e74f8ee6a4c06
SRB-Paljor <sbeckon2017@gmail.com>
2026-04-10 10:49:05 -0400

    Removed thrid offending form field, will convert on no error.

--- src/jurisdiction/California/maps.ts
+++ src/jurisdiction/California/maps.ts
@@ -470,7 +470,6 @@ export const nameChangeOnlyInfoMap: Formfill[] = [
     //eslint-disable-next-line
     fieldName: "NC-110[0].Page1[0].List7[0].LI7B[0].Subli7b[0].Subli7b2[0].Petitioner\.applies\.for\.item\.7b2\.ft[0]",
   }),
-*/
   (applicant) => ({
     text: formatDate(applicant.birthdate, {
       format: [DATE.MONTH, DATE.DAY, DATE.YEAR],
@@ -479,6 +478,7 @@ export const nameChangeOnlyInfoMap: Formfill[] = [
     //eslint-disable-next-line
     fieldName: "NC-110[0].Page1[0].List7[0].LI7B[0].Subli7b[0].Subli7b3[0].Petitioner\.applies\.for\.item\.7b3\.ft[0]",
   }),
+*/
   (applicant) => ({
     check: isMinor(applicant),
     fieldName: "NC-110[0].Page1[0].List7[0].LI7B[0].Subli7b[0].Subli7b3[0].eighteen[0]",
