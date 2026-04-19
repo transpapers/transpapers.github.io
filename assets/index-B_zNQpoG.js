b70e28c542078ace2da2b3f5713e874fb683a1c4
SRB-Paljor <SRB-Paljor@users.noreply.github.com>
2026-04-19 13:30:23 +0000

    deploy: 0b33d153246b158a264913f57fa79cd13d0d8aea

--- assets/index-B_zNQpoG.js
+++ assets/index-B_zNQpoG.js
@@ -41750,12 +41750,14 @@ const adultNameGenderOrderMap = [
   (applicant) => ({
     check: applicant.isChangingLegalName,
     fieldName: "NC-330[0].Page2[0].List10[0].Li10A[0].Checkbox10[0]"
-  }),
+  })
+  /*
   (applicant) => ({
     text: applicant.isChangingLegalName ? fullName(applicant.chosenName) : "",
     //eslint-disable-next-line
-    fieldName: "NC-330[0].Page2[0].List10[0].Li10A[0].TEXT.0.6.1.1.4.1.1.1.0.1.0.1.1.1.3.1.0[0]"
-  })
+    fieldName: "NC-330[0].Page2[0].List10[0].Li10A[0].TEXT\.0\.6\.1\.1\.4\.1\.1\.1\.0\.1\.0\.1\.1\.1\.3\.1\.0[0]",
+  }),
+  */
 ];
 const minorNameGenderOrderMap = [
   (applicant) => ({
