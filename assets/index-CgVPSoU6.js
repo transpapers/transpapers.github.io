ab12d4ff4be99671a8cfeca064da43c50da09967
SRB-Paljor <SRB-Paljor@users.noreply.github.com>
2026-05-01 15:16:12 +0000

    deploy: 2e80ad66d263a5b38174c3f2e349e804c24bd68b

--- assets/index-CgVPSoU6.js
+++ assets/index-CgVPSoU6.js
@@ -41129,8 +41129,7 @@ const adultNameGenderPetitionMap = [
   }),
   (applicant) => ({
     text: fullName(applicant.legalName),
-    fieldName: "NC-300[0].Page1[0].Caption[0].TitlePartyName[0].TextField1[0]",
-    font: 9
+    loc: { x: 167, y: 289 }
   }),
   (applicant) => ({
     check: applicant.isChangingLegalName,
